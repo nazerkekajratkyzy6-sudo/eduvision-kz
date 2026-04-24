1d7962a38ce7f81020c233690d8234b8aaabe98d
nazerkekajratkyzy6-sudo <nazerkekajratkyzy6@gmail.com>
2026-04-24 14:46:03 +0500

    Update story.js

--- js/story.js
+++ js/story.js
@@ -4,4 +4,9 @@ const id = params.get("id");
 const story = STORIES.find(s => s.id === id);
 
 document.getElementById("title").innerText = story.title;
-document.getElementById("text").innerText = story.text;
+document.getElementById("meta").innerText =
+  `${story.grade} сынып • ${story.type} • ${story.theme}`;
+
+// перенос строк сақтаймыз
+document.getElementById("text").innerHTML =
+  story.text.replace(/\n/g, "<br><br>");
