90c37bcf5b304c5401e1af6545a68d34e6be2f3f
nazerkekajratkyzy6-sudo <nazerkekajratkyzy6@gmail.com>
2026-04-24 14:45:03 +0500

    Update stories.js

--- js/stories.js
+++ js/stories.js
@@ -2,12 +2,14 @@ const container = document.getElementById("stories");
 
 STORIES.forEach(story => {
   const div = document.createElement("div");
-  div.className = "card";
+  div.className = "story-card";
 
   div.innerHTML = `
+    <div class="icon">📖</div>
     <h3>${story.title}</h3>
-    <p>${story.theme}</p>
-    <a href="story.html?id=${story.id}">Оқу</a>
+    <p class="meta">${story.grade} сынып • ${story.type}</p>
+    <p class="theme">${story.theme}</p>
+    <a class="btn" href="story.html?id=${story.id}">Оқу</a>
   `;
 
   container.appendChild(div);
